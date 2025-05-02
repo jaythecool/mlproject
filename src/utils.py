ce7aa61548b3afdc443a6cef790a775769f00c89
jaythecool <jayeshvengurlekar7@gmail.com>
2025-05-02 13:41:59 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -1,28 +1,9 @@
-# import os
-# import sys
-# import numpy as np
-# import pandas as pd
-# import dill
-# from src.exception import CustomException
-# from src.logger import logging
-
-# def save_obj(file_path,obj):
-#     try:
-#         dir_path = os.path.dirname(file_path)
-#         os.makedirs(dir_path,exist_ok=True)
-        
-#         with open(file_path,"wb") as file_obj:
-#             dill.dump(obj,file_obj)
-
-#         logging.info(f"Object saved successfully at {file_path}")
-#     except Exception as e:
-#         raise CustomException(e,sys)
-
 import dill
 import os
 import sys
 from src.logger import logging
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_obj(file_path, obj):
     try:
@@ -37,3 +18,22 @@ def save_obj(file_path, obj):
     except Exception as e:
         logging.error(f"Error occurred while saving object: {e}")
         raise CustomException(e, sys)
+
+def evaluate_model(x_train,x_test,y_train,y_test,models):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(x_train,y_train)
+
+            y_pred_train = model.predict(x_train)
+            y_pred_test = model.predict(x_test)
+
+            train_model_score = r2_score(y_train,y_pred_train)
+            test_model_score = r2_score(y_test,y_pred_test)
+
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
